8a30ba66b511e3d4809a3756c7f476d5ada1e1a8
Devang-shah2023 <shahdevang1232@gmail.com>
2024-08-22 08:54:19 +0530

    greeting thread multithreading learning

--- Lab-9/Lab91alter.java
+++ Lab-9/Lab91alter.java
@@ -1,9 +1,9 @@
-public class Lab91 {
+public class Lab91alter {
     public static void main(String[] args) {
         GreetingThread morningGreeting = new GreetingThread("1", 1000);
-        GreetingThread afternoonGreeting = new GreetingThread("2", 2000);
-        GreetingThread eveningGreeting = new GreetingThread("3", 3000);
-        GreetingThread nightGreeting = new GreetingThread("4", 4000);
+        GreetingThread afternoonGreeting = new GreetingThread("2", 1000);
+        GreetingThread eveningGreeting = new GreetingThread("3", 1000);
+        GreetingThread nightGreeting = new GreetingThread("4", 1000);
 
         Thread morningThread = new Thread(morningGreeting);
         Thread afternoonThread = new Thread(afternoonGreeting);
